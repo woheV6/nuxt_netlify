076e61fea01b1436a430f8d9ecea531e200927a0
hehong <wohev5@163.com>
2018-12-15 10:21:48 +0800

    添加美女图片

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
 <div>
-  <ul>
+  <ul id="menu">
     <li><nuxt-link :to="{name:'index'}">HOME</nuxt-link></li>
     <li><nuxt-link :to="{name:'about'}">ABOUT</nuxt-link></li>
     <li><nuxt-link :to="{name:'news',params:{newsId:123}}">NREWS</nuxt-link></li>
     <li><nuxt-link :to="{name:'asyncData'}">asyncData</nuxt-link></li>
   </ul>
   <div class="diss">
-    <img src='~static/diss.jpg'>
-  </div>
-  <div class="img_box">
-
+    <img src='~static/nizi.jpg'>
   </div>
 </div>
 </template>
@@ -27,6 +24,15 @@ export default {
   background: url(~static/diss.jpg);
   width: 200px;
   height:200px
+}
+#menu{
+  display: flex;
+}
+#menu li{
+  padding:0;
+  margin:0;
+  list-style:none;
+width: 100px;
 }
 </style>
 
